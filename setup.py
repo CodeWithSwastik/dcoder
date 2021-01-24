2b937d760ea27efb779107168b68ed8a2ff3b617
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-24 18:14:14 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='dcoder',
+  version='0.1.0',
+  description='A module that can decode/encode text in various ciphers.',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/CodeWithSwastik/dcoder",  
+  author='Swas.py',
+  author_email='cwswas.py@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='decoder,encoder,ciphers', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
