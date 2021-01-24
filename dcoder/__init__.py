2edf2673aa7268214342b7867adb0d3bd5d7e2da
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-24 09:20:33 +0530

    Added functions for en/decoding RailFence
    Added functions for encoding/decoding the RailFence/Zig-Zag cipher.

--- dcoder/__init__.py
+++ dcoder/__init__.py
@@ -79,5 +79,91 @@ def atbash2text(encrypted_text):
 def text2atbash(text):
 	return atbash2text(text)
 
+def reverse(text):
+	return text[::-1]
 
+def text2railfence(text, key = 3): 
 
+	rail = [['\n' for i in range(len(text))] 
+				  for j in range(key)] 
+	  
+	dir_down = False
+	row, col = 0, 0
+	  
+	for i in range(len(text)): 
+		  
+ 
+		if (row == 0) or (row == key - 1): 
+			dir_down = not dir_down 
+		  
+
+		rail[row][col] = text[i] 
+		col += 1
+		   
+		if dir_down: 
+			row += 1
+		else: 
+			row -= 1
+
+	result = [] 
+	for i in range(key): 
+		for j in range(len(text)): 
+			if rail[i][j] != '\n': 
+				result.append(rail[i][j]) 
+	return "" . join(result) 
+	  
+
+def railfence2text(cipher, key = 3): 
+
+	rail = [['\n' for i in range(len(cipher))]  
+				  for j in range(key)] 
+	  
+
+	dir_down = None
+	row, col = 0, 0
+	  
+
+	for i in range(len(cipher)): 
+		if row == 0: 
+			dir_down = True
+		if row == key - 1: 
+			dir_down = False
+		  
+		rail[row][col] = '*'
+		col += 1
+		  
+
+		if dir_down: 
+			row += 1
+		else: 
+			row -= 1
+	index = 0
+	for i in range(key): 
+		for j in range(len(cipher)): 
+			if ((rail[i][j] == '*') and
+			   (index < len(cipher))): 
+				rail[i][j] = cipher[index] 
+				index += 1
+		  
+
+	result = [] 
+	row, col = 0, 0
+	for i in range(len(cipher)): 
+		  
+ 
+		if row == 0: 
+			dir_down = True
+		if row == key-1: 
+			dir_down = False
+			  
+ 
+		if (rail[row][col] != '*'): 
+			result.append(rail[row][col]) 
+			col += 1
+			  
+
+		if dir_down: 
+			row += 1
+		else: 
+			row -= 1
+	return "".join(result)
