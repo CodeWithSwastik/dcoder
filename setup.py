b418bdad6f2d2ea9b8533e7b37d373cdc5bf1791
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-25 21:15:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setup(
   name='dcoder',
-  version='0.1.1',
+  version='0.1.2',
   description='A module that can decode/encode text in various ciphers.',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
   long_description_content_type = "text/markdown",
