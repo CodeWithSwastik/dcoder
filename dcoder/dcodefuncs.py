2aa8e04551a7b39d617eac57ee6eb82172a7f475
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-25 21:12:08 +0530

    Fixes #1
    Reorganized the code, added appropriate comments and fixed #1

--- dcoder/dcodefuncs.py
+++ dcoder/dcodefuncs.py
@@ -1,27 +1,15 @@
 import string
 
 
-#Encoding/Decoding
-
-def bin2text(binary_text):
-	plain_string = "".join([chr(int(x, 2)) for x in binary_text.split(" ")])
-	return plain_string 
+#Encoding
 
 def text2bin(text):
 	bin_string = " ".join(format(ord(x), 'b') for x in text)
-	return bin_string 
-
-def oct2text(oct_text):
-	plain_string = ''.join([chr(int(''.join(x), 8)) for x in oct_text.split(" ")])
-	return plain_string 	
+	return bin_string
 
 def text2oct(text):
 	oct_string = " ".join(format(ord(x), "o") for x in text)
 	return oct_string 
-
-def hex2text(hex_text):
-	plain_string = ''.join([chr(int(''.join(x), 16)) for x in hex_text.split(" ")])
-	return plain_string 	
 
 def text2hex(text):
 	hex_string = " ".join(format(ord(x), "x") for x in text)
@@ -31,13 +19,56 @@ def text2ascii(text):
 	ascii_string = " ".join(format(ord(x))  for x in text)
 	return ascii_string
 
+
+#Decoding
+
+noSpaceError = "No spaces were found in the {}. The output might be not be correct if the input text is not correctly formatted."
+
+
+def bin2text(binary_text):
+
+	#Raise exception if there are no spaces in the text
+	if not " " in binary_text and len(binary_text)>8:
+		raise ValueError(noSpaceError.format("binary_text"))
+
+	#Converts the bin to text
+	plain_string = "".join([chr(int(x, 2)) for x in binary_text.split(" ")])
+	return temp_str 
+
+
+def oct2text(oct_text):
+
+	#Raise exception if there are no spaces in the text
+	if not " " in oct_text and len(oct_text)>3:
+		raise ValueError(noSpaceError.format("oct_text"))
+
+	#Converts the oct to text
+	plain_string = ''.join([chr(int(''.join(x), 8)) for x in oct_text.split(" ")])
+	return plain_string 	
+
+def hex2text(hex_text):
+
+	#Raise exception if there are no spaces in the text
+	if not " " in hex_text and len(hex_text)>2:
+		raise ValueError(noSpaceError.format("hex_text"))
+
+	#Converts the hex to text
+	plain_string = ''.join([chr(int(''.join(x), 16)) for x in hex_text.split(" ")])
+	return plain_string 	
+
 def ascii2text(ascii_text):
+
+	#Raise exception if there are no spaces in the text
+	if not " " in ascii_text and len(ascii_text)>3:
+		raise ValueError(noSpaceError.format("ascii_text"))
+
+	#Converts the ascii to plain text
 	plain_string = ''.join([chr(int(''.join(x))) for x in ascii_text.split(" ")])
 	return plain_string
 
 
 
-#Encryption/Decryption
+#Encryption
 
 def text2caesar(text,shift = 3): 
 	result = "" 
@@ -52,6 +83,44 @@ def text2caesar(text,shift = 3):
 		else:
 			result += char
 	return result 
+
+def text2atbash(text):
+	return atbash2text(text)
+
+def text2railfence(text, key = 3): 
+
+	rail = [['\n' for i in range(len(text))] 
+				  for j in range(key)] 
+	  
+	dir_down = False
+	row, col = 0, 0
+	  
+	for i in range(len(text)): 
+		  
+ 
+		if (row == 0) or (row == key - 1): 
+			dir_down = not dir_down 
+		  
+
+		rail[row][col] = text[i] 
+		col += 1
+		   
+		if dir_down: 
+			row += 1
+		else: 
+			row -= 1
+
+	result = [] 
+	for i in range(key): 
+		for j in range(len(text)): 
+			if rail[i][j] != '\n': 
+				result.append(rail[i][j]) 
+	return "" . join(result) 
+	  
+
+
+
+#Decryption
 
 def caesar2text(encrypted_text,shift = 3):
 	translated = ""
@@ -97,40 +166,6 @@ def atbash2text(encrypted_text):
 			translated += s
 
 	return translated
-
-def text2atbash(text):
-	return atbash2text(text)
-
-def text2railfence(text, key = 3): 
-
-	rail = [['\n' for i in range(len(text))] 
-				  for j in range(key)] 
-	  
-	dir_down = False
-	row, col = 0, 0
-	  
-	for i in range(len(text)): 
-		  
- 
-		if (row == 0) or (row == key - 1): 
-			dir_down = not dir_down 
-		  
-
-		rail[row][col] = text[i] 
-		col += 1
-		   
-		if dir_down: 
-			row += 1
-		else: 
-			row -= 1
-
-	result = [] 
-	for i in range(key): 
-		for j in range(len(text)): 
-			if rail[i][j] != '\n': 
-				result.append(rail[i][j]) 
-	return "" . join(result) 
-	  
 
 def railfence2text(cipher, key = 3): 
 
@@ -202,12 +237,11 @@ def capitalLettersCipher(ciphertext):
 
 	Example:
 
-	Cipher Text:
-	dogs are cuter than HorsEs in a LooP. 
-
+	Cipher Text: dogs are cuter than HorsEs in a LooP. 
 	Decoded Text: HELP """  
 
 	return "".join([i for i in ciphertext if i.isupper()])
+
 
 def firstLetterCipher(ciphertext):
 	""" 
@@ -219,5 +253,3 @@ def firstLetterCipher(ciphertext):
 	Decoded text: Help """  
 
 	return "".join([i[0] for i in ciphertext.split(" ")])
-
-
