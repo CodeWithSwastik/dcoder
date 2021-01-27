a2993ea2854e18c299d88b1fd48bf06d2183cd39
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-27 17:56:00 +0530

    Release 0.2.1 (#8)
    * Update README.md
    
    * Added railfenceBruteforce()
    
    * Update dcodefuncs.py
    
    * Shifting Encode funcs to a separate file
    
    * Create encodefuncs.py
    
    * Update dcodefuncs.py
    
    * Update encodefuncs.py
    
    * Update dcodefuncs.py
    
    * Update __init__.py
    
    * Update setup.py
    
    * Update README.md
    
    * Update CHANGELOG.md
    
    * Added docstrings
    
    * Added docstrings
    
    * Added test docs
    
    * Delete docs directory
    
    * Added documentation
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update setup.py
    
    * Update __init__.py
    
    * Update CHANGELOG.md
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
 
 setup(
   name='dcoder',
-  version='0.2.0',
+  version='0.2.1',
   description='A module that can decode/encode text in various ciphers.',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
   long_description_content_type = "text/markdown",
-  url = "https://github.com/CodeWithSwastik/dcoder",  
+  url = "https://github.com/CodeWithSwastik/dcoder", 
+  project_urls={
+   "Documentation": "https://dcoder.readthedocs.io/en/latest/",
+   "Issue tracker": "https://github.com/CodeWithSwastik/dcoder/issues",
+   },
   author='Swas.py',
   author_email='cwswas.py@gmail.com',
   license='MIT', 
