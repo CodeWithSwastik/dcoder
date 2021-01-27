ef7f9279a726b13e34793e48f355dfd2d3e12b78
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-01-27 18:06:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   name='dcoder',
   version='0.2.1',
   description='A module that can decode/encode text in various ciphers.',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+  long_description=open('README.md').read(),
   long_description_content_type = "text/markdown",
   url = "https://github.com/CodeWithSwastik/dcoder", 
   project_urls={
